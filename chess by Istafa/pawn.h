a1772197fa0729514d98f1a6597a5b26773d147f
faisal shahzad <78543902+5aisal@users.noreply.github.com>
2026-05-09 14:48:32 +0500

    Implement move validation for Pawn class
    Added isValidMove method to Pawn class for move validation.

--- chess by Istafa/pawn.h
+++ chess by Istafa/pawn.h
@@ -1,5 +1,6 @@
 #ifndef pawn_h
 #define pawn_h
+#include <cmath>
 #include "peice.h"
 
 class Pawn:public Peice {
@@ -10,6 +11,19 @@ public:
 
 			return 'p';
 	}
+
+	bool isValidMove(int sr, int sc, int dr, int dc) {
+        int dir = white ? -1 : 1;
+        int startRow = white ? 6 : 1;
+
+        if (dc == sc && dr == sr + dir){ return true; }
+
+        if (dc == sc && sr == startRow && dr == sr + (2 * dir)){ return true; }
+
+        if (abs(dc - sc) == 1 && dr == sr + dir){ return true; }
+
+        return false;
+    }
 };
 
 #endif pawn.h
