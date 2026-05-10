b1040e8c4db9fc27244711bf1b0c3539713f3865
Asmad Nadeem <asmadnadeem53@gmail.com>
2026-05-10 21:39:17 +0500

    Enable user input for chess moves
    Added user input for move coordinates in chess game.

--- chess by Istafa/chess.cpp
+++ chess by Istafa/chess.cpp
@@ -5,10 +5,13 @@ using namespace std;
 int main() {
     Board chessboard;
     bool whiteTurn = true;
+    int sr, sc, dr, dc;
 
     cout << "  === CHESS GAME ===" << endl;
     chessboard.display();
     cout << (whiteTurn ? "WHITE's turn" : "BLACK's turn") << endl;
+    cout << "Enter move (sr sc dr dc): ";
+    cin >> sr >> sc >> dr >> dc;
 
     return 0;
 }
