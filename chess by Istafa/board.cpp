8ea0a5e047de8501539ef2c56a4d843c99772ff5
Asmad Nadeem <asmadnadeem53@gmail.com>
2026-05-10 21:42:33 +0500

    Enhance display method with headers
    Updated display method to include row and column headers.

--- chess by Istafa/board.cpp
+++ chess by Istafa/board.cpp
@@ -34,7 +34,10 @@ void Board::setupBoard() {
 
 void Board::display() {
     cout << endl;
+    cout << "    0 1 2 3 4 5 6 7" << endl;
+    cout << "   -----------------" << endl;
     for (int i = 0; i < 8; i++) {
+        cout << " " << i << " | ";
         for (int j = 0; j < 8; j++) {
             if (BOARD[i][j] == NULL)
                 cout << ". ";
