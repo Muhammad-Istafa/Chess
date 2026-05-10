09c15c07b65ec676441f69ce7b43c81555b912de
Asmad Nadeem <asmadnadeem53@gmail.com>
2026-05-10 21:37:56 +0500

    Add turn system and player labels
    Added turn indication for the chess game.

--- chess by Istafa/chess.cpp
+++ chess by Istafa/chess.cpp
@@ -1,10 +1,14 @@
 #include "board.h"
+#include <iostream>
+using namespace std;
 
 int main() {
-
     Board chessboard;
+    bool whiteTurn = true;
 
+    cout << "  === CHESS GAME ===" << endl;
     chessboard.display();
+    cout << (whiteTurn ? "WHITE's turn" : "BLACK's turn") << endl;
 
     return 0;
 }
