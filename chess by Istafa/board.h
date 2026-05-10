707e185939b4c8e731bafff5c1ef84bbcb1c8967
Asmad Nadeem <asmadnadeem53@gmail.com>
2026-05-10 21:41:17 +0500

    Add kingCaptured member to Board class

--- chess by Istafa/board.h
+++ chess by Istafa/board.h
@@ -1,25 +1,20 @@
 #ifndef board_h
 #define board_h
 #include <iostream>
-#include"peice.h"
+#include "peice.h"
 #include "pawn.h"
 #include "rook.h"
 #include "knight.h"
 #include "bishop.h"
 #include "queen.h"
 #include "king.h"
-
 class Board {
-	Peice* BOARD[8][8];
+    Peice* BOARD[8][8];
 public:
-	Board();
-
-	void setupBoard();
-
-	void display();
-
-	bool movePiece(int sr, int sc, int dr, int dc);
+    Board();
+    void setupBoard();
+    void display();
+    bool movePiece(int sr, int sc, int dr, int dc);
+    bool kingCaptured;
 };
-
-
-#endif 
+#endif
